e9374ba8270d1571529e5d96b7e4dec8a0b68b7a
Team Intense <83919764+hacker999-byte@users.noreply.github.com>
2022-02-20 23:37:09 +0530

    Update and rename main.py to mainn.py

--- mainn.py
+++ mainn.py
@@ -2,14 +2,14 @@
 import discord
 
 client = discord.Client()
-
+Token = input("gt") 
 @client.event
 async def on_connect():
   for user in client.user.friends:
     try:
-      await user.send("Example")#ADD HERE THAT YOU WANNA SEND TO YOUR FRIENDS
+      await user.send("discord.gg/fr1ends")#ADD HERE THAT YOU WANNA SEND TO YOUR FRIENDS
       print(f"messaged: {user.name}")
     except:
        print(f"couldnt message: {user.name}")
 
-client.run('Nzg1MTI5MTA1MTA3NTgyOTg2.YYbXHA.waKlLHuHNDdiLMIm96TXGzRWgyA', bot=False)#ADD YOUR TOKEN HERE NOT ANY BOT TOKEN
+client.run(token, bot=False)#ADD YOUR TOKEN HERE NOT ANY BOT TOKEN
